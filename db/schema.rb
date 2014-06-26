8f423508a9d969833bcf5ca9a0d7a9a0b82d04b2
Gabriel Rodriguez <gaboxbluestyle@gmail.com>
2014-06-26 10:04:34 -0500

    Migración de Base de datos
    Subir imágenes Habilitado

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140601212024) do
+ActiveRecord::Schema.define(:version => 20140626145036) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "titulo"
+    t.string   "extension"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "usuarios", :force => true do |t|
     t.string   "username",         :null => false
