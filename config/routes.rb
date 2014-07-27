c033486e1c1de19475f90bf4cd128d89df8e8fa7
Gabriel Rodriguez <gaboxbluestyle@gmail.com>
2014-07-27 17:01:10 -0500

    Revert "arreglo nosotros"
    This reverts commit b62549a25b8797068c2da1c13e97de93ab7b0eb9.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Smiles4usv3::Application.routes.draw do
-  get "nosotros/index"
-
-  get "contacto/index"
-
   resources :comentarios
-
 
   get "favoritos/favoritos"
 
+  get "nosotros/index"
 
   get "password_resets/create"
 
